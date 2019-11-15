1736301bd455c3588a5c196a149d12815b3cfcf2
gongru <gongru0521@163.com>
2019-11-15 16:41:07 +0800

    test

--- SeleniumPageObject1031/src/test/java/com/test/demo/LanchTest.java
+++ SeleniumPageObject1031/src/test/java/com/test/demo/LanchTest.java
@@ -1,12 +1,14 @@
 package com.test.demo;
 
+import static org.junit.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 
 public class LanchTest {
 	
 	@Test
 	public void test001(){
-		
+		 assertTrue( true );
 		
 		
 	}
