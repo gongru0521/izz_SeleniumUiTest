291cdb31fa6d0f270efc93cf46fdce50c21d3737
gongru <gongru0521@163.com>
2019-11-18 16:56:19 +0800

    no message

--- SeleniumPageObject1031/src/test/java/com/test/demo/LanchTest.java
+++ SeleniumPageObject1031/src/test/java/com/test/demo/LanchTest.java
@@ -1,12 +1,14 @@
 package com.test.demo;
 
 import static org.junit.Assert.assertTrue;
+import io.qameta.allure.Feature;
 
 import org.testng.annotations.Test;
 
 public class LanchTest {
 	
 	@Test
+	@Feature("test001")
 	public void test001(){
 		 assertTrue( true );
 		 System.out.println("test001");
