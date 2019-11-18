8f7ad4d659ff15409ad2db98ff2586e04c328d39
gongru <gongru0521@163.com>
2019-11-18 14:01:17 +0800

    no message

--- SeleniumPageObject1031/src/test/java/com/test/SeleniumPageObject1031/AppTest.java
+++ SeleniumPageObject1031/src/test/java/com/test/SeleniumPageObject1031/AppTest.java
@@ -7,19 +7,19 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-    @Test
-    public void shouldAnswerWithTrue2()
-    {
-        assertTrue( true );
-    }
-}
+//public class AppTest 
+//{
+//    /**
+//     * Rigorous Test :-)
+//     */
+//   // @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//       // assertTrue( true );
+//    }
+//   // @Test
+//    public void shouldAnswerWithTrue2()
+//    {
+//        assertTrue( true );
+//    }
+//}
